30d69b19de34209e25902793c18a0fd618f3eb2d
Amos Wenger <amoswenger@gmail.com>
2019-11-08 12:43:13 +0100

    Initial async I/O tryouts
    Introduce second crate, `cbidiff`, to be a command-line frontend to the
    bidiff library. Use pico-args for argument parsing, async-std for async
    I/O, env_logger+log for logging.
    
    Read both files fully for now, doesn't actually diff anything yet.

---
+++ bidiff/src/lib.rs
@@ -0,0 +1,21 @@
+use async_std::{prelude::*, io::Read, future::try_join};
+use std::pin::Pin;
+use log::*;
+
+/// Diff two files
+pub async fn diff(mut older: Pin<&mut dyn Read>, mut newer: Pin<&mut dyn Read>) -> Result<(), async_std::io::Error>
+{
+    let mut obuf = Vec::new();
+    let mut nbuf = Vec::new();
+
+    {
+        let a = older.read_to_end(&mut obuf);
+        let b = newer.read_to_end(&mut nbuf);
+        try_join!(a, b).await?;
+    }
+
+    info!("older is {} bytes", obuf.len());
+    info!("newer is {} bytes", obuf.len());
+
+    Ok(())
+}
